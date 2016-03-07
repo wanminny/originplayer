b24eefdfca61f5015517e8264114663456f0805d
wanmin <min.wan@yoho.cn>
2016-03-07 12:39:40 +0800

    Initial Commit

---
+++ 原生播放器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  原生播放器
+//
+//  Created by wan on 16/3/7.
+//  Copyright © 2016年 wan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
